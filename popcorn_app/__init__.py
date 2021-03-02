92c50ca5c51af04df3b7b3414eac4f38b96081d3
Xila Luna <xilaluna2@gmail.com>
2021-03-01 20:46:02 -0800

    blueprints auth and main

--- popcorn_app/__init__.py
+++ popcorn_app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from popcorn_app.config import Config
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.config.from_object(Config)
 
 
-from books_app.main.routes import main as main_routes  # nopep8
+from popcorn_app.main.routes import main as main_routes  # nopep8
 app.register_blueprint(main_routes)
+
+from popcorn_app.auth.routes import auth as auth_routes  # nopep8
+app.register_blueprint(auth_routes)
---
+++ popcorn_app/main/forms.py
@@ -0,0 +1,2 @@
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from popcorn_app import app, db
