a37dd121ff024c297c8aefeded75f37c77f16877
Xila Luna <xilaluna2@gmail.com>
2021-03-01 20:35:17 -0800

    route files added

--- popcorn_app/__init__.py
+++ popcorn_app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from popcorn_app.config import Config
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config.from_object(Config)
+
+
+from books_app.main.routes import main as main_routes  # nopep8
+app.register_blueprint(main_routes)
--- popcorn_app/config.py
+++ popcorn_app/config.py
@@ -3,4 +3,9 @@ import os
 
 
 class Config:
-    pass
+    """Set environment variables."""
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    FLASK_ENV = 'development'
